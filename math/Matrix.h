7bfa0225d370ba6379961f557ba81b6593e44353
jfern2011 <jfern2011@gmail.com>
2018-05-14 22:18:16 -0700

    Updated the Matrix and Vector classes

---
+++ math/Matrix.h
@@ -0,0 +1,527 @@
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+#include <cstddef>
+#include <stdexcept>
+
+#include "abort.h"
+
+/**
+ * @class Matrix
+ *
+ * Represents an NxM matrix. Elements are placed in row-major order
+ */
+template <size_t N, size_t M>
+class Matrix
+{
+
+public:
+
+	Matrix(double init_val = 0);
+
+	Matrix(const double* data);
+
+	virtual ~Matrix() {};
+
+    double& operator()(size_t row, size_t col);
+
+    double  operator()(size_t row, size_t col) const;
+
+	Matrix  operator+(const Matrix& rhs) const;
+
+	Matrix& operator+=(const Matrix& rhs);
+
+	Matrix  operator-(const Matrix& rhs) const;
+
+	Matrix& operator-=(const Matrix& rhs);
+
+	Matrix  operator*(double scale) const;
+
+	Matrix& operator*=(double scale);
+
+	Matrix  operator/(double scale) const;
+
+	Matrix& operator/=(double scale);
+
+	template <size_t C>
+	Matrix operator*(const Matrix<M,C>& rhs) const;
+
+	template <size_t C>
+	Matrix& operator*=(const Matrix<M,C>& rhs);
+
+	bool operator==( const Matrix& rhs ) const;
+
+	size_t ncols() const;
+
+	size_t nrows() const;
+
+	Matrix& identify();
+
+	Matrix& zeroify();
+
+	bool set_data(const double* data);
+
+	template <size_t R, size_t C>
+	Matrix<R,C> sub(size_t row, size_t col) const;
+
+	Matrix<M,N> transpose() const;
+
+protected:
+
+	/**
+	 * The underlying buffer
+	 */
+	double _data[N*M];
+};
+
+/**
+ * Constructor (1)
+ *
+ * @param[in] init_val Initialize by setting all entries to this
+ */
+template <size_t N, size_t M>
+inline Matrix<N,M>::Matrix( double init_val )
+{
+	for (size_t i = 0; i < N * M; i++)
+		_data[i] = init_val;
+}
+
+/**
+ * Constructor (2)
+ *
+ * @param [in] data Initialize the matrix by pulling values from
+ *                  this in row-major order
+ */
+template <size_t N, size_t M>
+inline Matrix<N,M>::Matrix(const double* data)
+{
+	if (data == nullptr)
+	{
+		throw std::invalid_argument( "null pointer exception!" );
+	}
+
+	for (size_t i = 0; i < N * M; i++)
+		_data[i] = data[i];
+}
+
+/**
+ * Indexing operator
+ *
+ * @param[in] row Get the element at this row
+ * @param[in] col Get the element at this column
+ *
+ * @return A reference to the target element
+ */
+template <size_t N, size_t M>
+inline double& Matrix<N,M>::operator()(size_t row, size_t col)
+{
+	if (N <= row || M <= col)
+	{
+		char msg[256];
+		std::snprintf(msg, 256, 
+			"%s in %s, line %d: <N,M> = <%zu,%zu>, (row,col) = (%zu,%zu)\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			N, M, row, col);
+
+		throw std::out_of_range( msg );
+	}
+
+	return _data[row * M + col];
+}
+
+/**
+ * Indexing operator (const overload)
+ *
+ * @param[in] row Get the element at this row
+ * @param[in] col Get the element at this column
+ *
+ * @return A reference to the target element
+ */
+template <size_t N, size_t M>
+inline double Matrix<N,M>::operator()(size_t row, size_t col) const
+{
+	if (N <= row || M <= col)
+	{
+		char msg[256];
+		std::snprintf(msg, 256, 
+			"%s in %s, line %d: <N,M> = <%zu,%zu>, (row,col) = (%zu,%zu)\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			N, M, row, col);
+
+		throw std::out_of_range( msg );
+	}
+
+	return _data[row * M + col];
+}
+
+/**
+ * Element-wise addition (1)
+ *
+ * @param[in] rhs The matrix to add to *this
+ *
+ * @return The matrix sum
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator+(const Matrix& rhs) const -> Matrix
+{
+	Matrix<N,M> temp;
+
+	for (size_t i = 0; i < N*M; i++)
+	{
+		temp._data[i] = _data[i] + rhs._data[i];
+	}
+
+	return temp;
+}
+
+/**
+ * Element-wise addition (2)
+ *
+ * @param[in] rhs The matrix to add to *this
+ *
+ * @return *this
+ */
+template<size_t N, size_t M>
+inline auto Matrix<N,M>::operator+=(const Matrix& rhs) -> Matrix&
+{
+	for (size_t i = 0; i < N*M; i++)
+		_data[i] += rhs._data[i];
+
+	return *this;
+}
+
+/**
+ * Element-wise subtraction (1)
+ *
+ * @param[in] rhs The matrix to subtract from *this
+ *
+ * @return The matrix difference
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator-(const Matrix& rhs) const-> Matrix
+{
+	Matrix<N,M> temp;
+
+	for (size_t i = 0; i < N*M; i++)
+	{
+		temp._data[i] = _data[i] - rhs._data[i];
+	}
+
+	return temp;
+}
+
+/**
+ * Element-wise subtraction (2)
+ *
+ * @param[in] rhs The matrix to subtract from *this
+ *
+ * @return *this
+ */
+template<size_t N, size_t M>
+inline auto Matrix<N,M>::operator-=(const Matrix& rhs) -> Matrix&
+{
+	for (size_t i = 0; i < N*M; i++)
+		_data[i] -= rhs._data[i];
+
+	return *this;
+}
+
+/**
+ * Multiplication by a scalar, with the scalar as the right-hand
+ * operand (1)
+ *
+ * @param[in] scale Scale the matrix by this amount
+ *
+ * @return The scalar-matrix product
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator*(double scale) const -> Matrix
+{
+	Matrix<N,M> temp;
+
+	for (size_t i = 0; i < N*M; i++)
+	{
+		temp._data[i] = _data[i] * scale;
+	}
+
+	return temp;
+}
+
+/**
+ * Multiplication by a scalar, with the scalar as the right-hand
+ * operand (2)
+ *
+ * @param[in] scale Scale the matrix by this amount
+ *
+ * @return *this
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator*=(double scale) -> Matrix&
+{
+	for (size_t i = 0; i < N*M; i++)
+		_data[i] *= scale;
+
+	return *this;
+}
+
+/**
+ * Division by a scalar (1)
+ *
+ * @param[in] scale The scalar to divide by
+ *
+ * @return The element-wise quotient
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator/(double scale) const -> Matrix
+{
+	Matrix<N,M> temp;
+
+	if (scale == 0.0)
+	{
+		throw std::overflow_error( "attempted to divide by zero!" );
+	}
+
+	for (size_t i = 0; i < N*M; i++)
+	{
+		temp._data[i] = _data[i] / scale;
+	}
+
+	return temp;
+}
+
+/**
+ * Division by a scalar (2)
+ *
+ * @param[in] scale The scalar to divide by
+ *
+ * @return *this
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::operator/=(double scale) -> Matrix&
+{
+	if (scale == 0.0)
+	{
+		throw std::overflow_error( "attempted to divide by zero!" );
+	}
+
+	for (size_t i = 0; i < N*M; i++)
+		_data[i] /= scale;
+
+	return *this;
+}
+
+/**
+ * Multiplication by a scalar, with the scalar as the left-hand
+ * operand
+ *
+ * @param[in] scale Scale the matrix by this amount
+ * @param[in] rhs   The matrix being scale
+ *
+ * @return The matrix-scalar product
+ */
+template <size_t N, size_t M>
+inline Matrix<N,M> operator*(double scale, const Matrix<N,M>& rhs)
+{
+	return (rhs * scale);
+}
+
+/**
+ * Matrix multiplication (1)
+ *
+ * @tparam C The number of columns in the right-hand operand
+ *
+ * @param [in] rhs Multiply by this (right-hand side) matrix
+ *
+ * @return The matrix product
+ */
+template <size_t N, size_t M> template <size_t C>
+inline auto Matrix<N,M>::operator*(const Matrix<M,C>& rhs)
+	const -> Matrix
+{
+	Matrix<N,C> temp;
+	for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < C; j++)
+        {
+            for (size_t k = 0; k < M; k++)
+            {
+                temp(i,j) += this->operator()(i,k) * rhs(k,j);
+            }
+        }
+    }
+
+	return temp;
+}
+
+/**
+ * Matrix multiplication (2)
+ *
+ * @tparam C The number of columns in the right-hand operand
+ *
+ * @param [in] rhs Multiply by this (right-hand side) matrix
+ *
+ * @return *this
+ */
+template <size_t N, size_t M> template <size_t C>
+inline auto Matrix<N,M>::operator*=(const Matrix<M,C>& rhs)
+	-> Matrix&
+{
+	return (*this = operator*(rhs));
+}
+
+/**
+ * Performs element-wise comparison to determine if two matrices
+ * are equal
+ *
+ * @param[in] rhs The matrix to compare against
+ *
+ * @return True if both are equal
+ */
+template <size_t N, size_t M>
+inline bool Matrix<N,M>::operator==(const Matrix& rhs) const
+{
+	for (size_t i = 0 ; i < N*M; i++)
+	{
+		if (_data[i] != rhs._data[i])
+			return false;
+	}
+
+	return true;
+}
+
+/**
+ * Get the number of columns in this matrix
+ *
+ * @return The number of columns
+ */
+template <size_t N, size_t M>
+inline size_t Matrix<N,M>::ncols() const
+{
+	return M;
+}
+
+/**
+ * Get the number of rows in this matrix
+ *
+ * @return The number of rows
+ */
+template <size_t N, size_t M>
+inline size_t Matrix<N,M>::nrows() const
+{
+	return N;
+}
+
+/**
+ * Set this matrix to the identity, preserving its dimensions
+ *
+ * @return *this
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::identify() -> Matrix&
+{
+	static_assert(N == M, "matrix must be square");
+
+	zeroify();
+
+	for (size_t i = 0; i < N; i++)
+		this->operator()(i,i) = 1.0;
+
+	return *this;
+}
+
+/**
+ * Set the elements of this matrix to zero
+ *
+ * @return *this
+ */
+template <size_t N, size_t M>
+inline auto Matrix<N,M>::zeroify() -> Matrix&
+{
+	for (size_t i = 0; i < N * M; i++)
+		_data[i] = 0.0;
+
+	return *this;
+}
+
+/**
+ * Re-assign the matrix data
+ *
+ * @param[in] data The set of values to be assigned to this matrix
+ *                 in row-major order
+ *
+ * @return True on success
+ */
+template <size_t N, size_t M>
+inline bool Matrix<N,M>::set_data(const double* data)
+{
+	AbortIfNot(data, false);
+
+	for (size_t i = 0; i < N*M; i++)
+		_data[i] = data[i];
+
+	return true;
+}
+
+/**
+ * Grab a block matrix from this matrix
+ *
+ * @tparam R The number of rows in the sub-matrix
+ * @tparam C The number of columns
+ *
+ * @param[in] row   The starting row of the block
+ * @param[in] col   The starting column
+ *
+ * @return A sub-matrix
+ */
+template <size_t N, size_t M> template <size_t R, size_t C>
+inline Matrix<R,C> Matrix<N,M>::sub(size_t row, size_t col) const
+{
+	static_assert(R < N && C < M, "");
+
+	if (row + R > N || col + C > M)
+	{
+		char msg[256];
+		std::snprintf(msg, 256,
+			"%s in %s, line %d: (row,col) = (%zu,%zu), <R,C> = <%zu,%zu>\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			row, col, R, C);
+
+		throw std::out_of_range( msg );
+	}
+
+	Matrix<R,C> temp;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		for (size_t j = 0; j < C; j++)
+		{
+			temp(i,j) =
+				this->operator()(i+row, j+col);
+		}
+	}
+
+	return temp;
+}
+
+/**
+ * Compute the transpose of this matrix
+ *
+ * @return The transpose of *this
+ */
+template <size_t N, size_t M>
+inline Matrix<M,N> Matrix<N,M>::transpose() const
+{
+	Matrix<M,N> ans;
+
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < M; j++)
+		{
+			ans(j,i) = this->operator()(i,j);
+		}
+	}
+
+	return ans;
+}
+
+#endif
---
+++ math/Vector.h
@@ -0,0 +1,264 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+#include <cmath>
+
+#include "Matrix.h"
+
+/**
+ * @class Vector
+ *
+ * Represents an Nx1 (column) vector. To work with row vectors, see
+ * \ref Matrix<1,N>
+ */
+template <size_t N>
+class Vector : public Matrix<N,1>
+{
+
+public:
+
+	Vector(double init_val = 0);
+
+	Vector(const Matrix<N,1>& mat);
+
+	Vector(const double* data);
+
+	~Vector();
+
+	Vector& operator=(const Matrix<N,1>& mat);
+
+	double& operator()(size_t index);
+
+	double operator()(size_t index) const;
+
+	Vector cross(const Vector& rhs) const;
+
+	double dot(const Vector& rhs) const;
+
+	size_t length() const;
+
+	double norm() const;
+
+	Matrix<3,3> skew() const;
+
+	template <size_t M>
+		Vector<M> sub(size_t index) const;
+};
+
+#endif
+
+/**
+ * Constructor (1)
+ *
+ * @param[in] init_val Initialize by setting all entries to this
+ */
+template <size_t N>
+inline Vector<N>::Vector(double init_val) : Matrix<N,1>(init_val)
+{
+}
+
+/**
+ * Constructor (2)
+ *
+ * @param [in] data Initialize the vector by pulling values from
+ *                  this
+ */
+template <size_t N>
+inline Vector<N>::Vector(const double* data) : Matrix<N,1>(data)
+{
+}
+
+/**
+ * Copy constructor
+ *
+ * @param[in] mat The \ref Matrix or Nx1 vector to copy
+ */
+template <size_t N>
+inline Vector<N>::Vector(const Matrix<N,1>& mat)
+	: Matrix<N,1>(mat)
+{
+}
+
+/**
+ * Destructor
+ */
+template <size_t N>
+inline Vector<N>::~Vector()
+{
+}
+
+/**
+ * Assignment operator
+ *
+ * @param[in] mat The \ref Matrix or Vector to assign to
+ *
+ * @return *this
+ */
+template <size_t N>
+inline auto Vector<N>::operator=(const Matrix<N,1>& mat) -> Vector&
+{
+	if (*this != &mat)
+		Matrix<N,1>::operator=(mat);
+
+	return *this;
+}
+
+/**
+ * Indexing operator
+ *
+ * @param[in] index Get the element at this index
+ *
+ * @return The element at index \a index
+ */
+template <size_t N>
+inline double& Vector<N>::operator()(size_t index)
+{
+	if (N <= index)
+	{
+		char msg[256];
+		std::snprintf(msg, 256, 
+			"%s in %s, line %d: N = %zu, index = %zu\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			N, index);
+
+		throw std::out_of_range( msg );
+	}
+
+	return this->_data[index];
+}
+
+/**
+ * Indexing operator (const overload)
+ *
+ * @param[in] index Get the element at this index
+ *
+ * @return The element at index \a index
+ */
+template <size_t N>
+inline double Vector<N>::operator()(size_t index) const
+{
+	if (N <= index)
+	{
+		char msg[256];
+		std::snprintf(msg, 256, 
+			"%s in %s, line %d: N = %zu, index = %zu\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			N, index);
+
+		throw std::out_of_range( msg );
+	}
+
+	return this->_data[index];
+}
+
+/**
+ * Vector cross product. This operation is only defined for
+ * 3-vectors
+ *
+ * @param[in] rhs The right-hand operand
+ *
+ * @return The cross product
+ */
+template <size_t N>
+inline auto Vector<N>::cross(const Vector& rhs) const -> Vector
+{
+	static_assert(N == 3,
+		"cross product is only defined for 3x1 vectors.");
+
+	return skew() * rhs;
+}
+
+/**
+ * Vector dot product
+ *
+ * @param[in] rhs The vector to take the dot product with
+ *
+ * @return The dot product
+ */
+template <size_t N>
+inline double Vector<N>::dot(const Vector& rhs) const
+{
+	double ans = 0;
+
+	for (int i = 0; i < N; i++)
+    {
+        ans += this->_data[i] * rhs._data[i];
+    }
+
+    return ans;
+}
+
+/**
+ * Get the number of elements in the vector
+ *
+ * @return The number of rows
+ */
+template <size_t N>
+inline size_t Vector<N>::length() const
+{
+	return N;
+}
+
+/**
+ * Compute the L^2 (Euclidean) norm
+ *
+ * @return The vector norm
+ */
+template <size_t N>
+inline double Vector<N>::norm() const
+{
+	return std::sqrt(dot(*this));
+}
+
+/**
+ * Compute the skew-symmetric matrix of a 3-vector
+ *
+ * @return The skew-symmetric matrix
+ */
+template <size_t N>
+inline Matrix<3,3> Vector<N>::skew() const
+{
+	static_assert(N == 3,
+		"skew() is only defined for 3x1 vectors.");
+
+	Matrix<3,3> ans;
+
+	ans(0,1) = -this->_data[2];
+    ans(0,2) =  this->_data[1];
+	ans(1,0) =  this->_data[2];
+	ans(1,2) = -this->_data[0];
+	ans(2,0) = -this->_data[1];
+	ans(2,1) =  this->_data[0];
+
+	return ans;
+}
+
+/**
+ * Retrieve a sub-vector
+ *
+ * @param[in] index The starting index from which to grab the
+ *                  smaller vector
+ *
+ * @return A sub-vector
+ */
+template <size_t N> template <size_t M>
+inline Vector<M> Vector<N>::sub(size_t index) const
+{
+	static_assert(M <= N,
+		"cannot take a sub-vector of a larger one.");
+
+	if (M + index > N)
+	{
+		char msg[256];
+		std::snprintf(msg, 256, 
+			"%s in %s, line %d: N = %zu, M = %zu, index = %zu\n",
+			__PRETTY_FUNCTION__, __FILE__, __LINE__,
+			N, M, index);
+
+		throw std::out_of_range( msg );
+	}
+
+	Vector<M> ans(&this->_data[index]);
+
+	return ans;
+}
