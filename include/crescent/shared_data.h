f81ce79306cba0daa3f8b82ca3c4f4de7fccc817
jfern2011 <jfern2011@gmail.com>
2022-11-26 11:33:46 -0500

    Add submodule dependencies

---
+++ include/crescent/shared_data.h
@@ -0,0 +1,15 @@
+/**
+ *  \file   shared_data.h
+ *  \author Jason Fernandez
+ *  \date   11/26/2022
+ */
+
+#ifndef CRESCENT_SHARED_DATA_H_
+#define CRESCENT_SHARED_DATA_H_
+
+namespace crescent {
+
+
+}  // namespace crescent
+
+#endif  // CRESCENT_SHARED_DATA_H_
