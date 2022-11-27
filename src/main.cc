95417fede5319edde7c7f1952a049f71b5de5f73
jfern2011 <jfern2011@gmail.com>
2022-11-26 20:43:03 -0500

    Add eigen to cmake build

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,16 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "Eigen/Dense"
+
 int main(int argc, char** argv) {
-    std::cout << "Hello, world!" << std::endl;
+    Eigen::MatrixXd matrix(2,2);
+
+    matrix(0,0) = 3.0;
+    matrix(1,0) = 2.5;
+    matrix(0,1) = -1.0;
+    matrix(1,1) = matrix(1,0) + matrix(0,1);
+
+    std::cout << matrix << std::endl;
     return EXIT_SUCCESS;
 }
